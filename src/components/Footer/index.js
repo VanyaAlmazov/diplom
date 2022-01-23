8101944fcc8603fb14a83b49c281df4d36cf1ae3
Ванек_Мажейко <mazheycko.vanyok@yandex.ru>
2022-01-23 20:39:14 +0300

    one

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import styles from "./style.module.scss";
+
+
+const Footer = () => {
+
+  return (
+    <div className={styles.footer}>
+      <div>FOOTER</div>
+    </div>
+  );
+};
+
+export default Footer;
